688eaad9bae66c4164b506246205a67e55ebb29e
TheXnumb <132187960+thexnumb@users.noreply.github.com>
2025-04-24 20:52:41 +0300

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -30,6 +30,8 @@ def format_markdown_table(articles):
 
 def write_readme(table_content):
     with open(README_FILE, "w", encoding="utf-8") as readme:
+        readme.write("# 📝 THEXWRITEUP\n\n")
+        readme.write("Store all the latest Medium Articles by specific tags for the Bug Bounty hunting and Security filed and show most 100 recent articles and Notify on the Discord server. \n\n")
         readme.write("## 🌐 Let's Connect\n\n")
         readme.write("[![Discord](https://img.shields.io/badge/Discord-@thexnumb-1DA1F2?style=flat&logo=discord&logoColor=white)](https://discord.gg/evffhtjWR7) [![Twitter](https://img.shields.io/badge/X-@thexsecurity-1DA1F2?style=flat&logo=twitter&logoColor=white)](https://x.com/thexsecurity) [![Telegram](https://img.shields.io/badge/Telegram-@thexsecurity-2CA5E0?style=flat&logo=telegram&logoColor=white)](https://t.me/thexsecurity) [![Instagram](https://img.shields.io/badge/Instagram-@thexnumb-E4405F?style=flat&logo=instagram&logoColor=white)](https://instagram.com/thexnumb) [![Infosec.exchange](https://img.shields.io/badge/Infosec.exchange-@thexnumb-E11BE9?style=flat&logo=mastodon&logoColor=white)](https://infosec.exchange/@thexnumb) [![LinkedIn](https://img.shields.io/badge/LinkedIn-Profile-blue?style=flat&logo=linkedin)](#) [![Medium](https://img.shields.io/badge/Medium-@thexnumb-black?style=flat&logo=medium)](https://medium.com/@thexnumb) [![Blogger](https://img.shields.io/badge/Blogger-TheXSecurity-FF5722?style=flat&logo=blogger&logoColor=white)](https://thexsecurity.blogspot.com/) [![YouTube](https://img.shields.io/badge/YouTube-@theXNumb-FF0000?style=flat&logo=youtube&logoColor=white)](https://www.youtube.com/@theXNumb/)\n\n")
         readme.write("🚀 **Let's hack the planet!** 🔥\n\n")
